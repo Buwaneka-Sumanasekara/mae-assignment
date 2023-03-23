0b105659c7f62ac2583e475c2e39f6c1456b3faa
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2023-03-23 23:33:23 +0530

    cors issue fixed

--- src/redux/modules/search.ts
+++ src/redux/modules/search.ts
@@ -14,7 +14,7 @@ const loadSuggestionsInProgress = () => ({ type: ActionTypes.SEARCHING });
 const loadSuggestionsEpic = (action$: any, state$: any) => action$.pipe(
     ofType(ActionTypes.SEARCH_VALUE_CHANGED),
     throttleTime(1000 as any),
-    mergeMap((action: any) => from(getAxios().get(`maps/api/place/autocomplete/json?key=${process.env.REACT_APP_GOOGLE_API_KEY}&input==${action.payload.value}`)).pipe(
+    mergeMap((action: any) => from(getAxios().get(`maps/api/place/autocomplete/json?key=${process.env.REACT_APP_GOOGLE_API_KEY}&input=${action.payload.value}`)).pipe(
         map((response: any) => loadSuggestionsSuccess(response?.predictions || [])),
         catchError((er) => loadSuggestionsFailed())
     )),
